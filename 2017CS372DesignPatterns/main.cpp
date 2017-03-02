2dab57284e232c88549494f9e897d5f19fde67ad
Chris Hartman <chris.m.hartman@gmail.com>
2017-03-01 15:01:41 -0900

    Initial Commit

---
+++ 2017CS372DesignPatterns/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2017CS372DesignPatterns
+//
+//  Created by Chris Hartman on 3/1/17.
+//  Copyright © 2017 Chris Hartman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
